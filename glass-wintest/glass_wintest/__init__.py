28027a963a3c2baeabd5bad13d6de98669c42103
Egil <egil.moller@freecode.no>
2019-09-28 01:29:41 +0200

    Use DEBUG logger in wintest

--- glass-wintest/glass_wintest/__init__.py
+++ glass-wintest/glass_wintest/__init__.py
@@ -30,7 +30,7 @@ with InfiniteGlass.Display() as display:
                 w = abs(current_coord[0] - current_win_start[0])
                 h = abs(current_coord[1] - current_win_start[1])
                 current_win["IG_COORDS"]=[x, y, w, h]
-                print("Resize win", current_win["IG_COORDS"])
+                InfiniteGlass.DEBUG("resize", "Resize win %s\n" %(current_win["IG_COORDS"],))
                 
     display.root.grab_button(
         Xlib.X.AnyButton, Xlib.X.Mod5Mask, False,
@@ -46,7 +46,7 @@ with InfiniteGlass.Display() as display:
 
         current_win_start = list(current_coord[:2])
         current_win["IG_COORDS"]=current_win_start + [0.01, 0.01]
-        print("New win", current_win["IG_COORDS"])
+        InfiniteGlass.DEBUG("new", "New win %s\n" % (current_win["IG_COORDS"],))
         
         with pkg_resources.resource_stream("glass_wintest", "wintest.svg") as f:
             current_win["DISPLAYSVG"]=f.read()
@@ -59,3 +59,4 @@ with InfiniteGlass.Display() as display:
         
         current_win = None
         
+    InfiniteGlass.DEBUG("init", "Wintest started\n")
