faae3ca7ef6a78d36ade36053676980c65628ea1
Egil <egil.moller@freecode.no>
2019-10-12 16:20:04 +0200

    Startup cleanup

--- glass-startup.sh
+++ glass-startup.sh
@@ -1,35 +1,17 @@
 #! /bin/bash
 
 exec > /tmp/glass.log 2>&1
-ulimit -c unlimited
 
-if true; then
-
-  export GLASS_SHADER_PATH=/usr/local/share/glass
-  export GLASS_ERROR_EXIT=True
-  export GLASS_ERROR_BAD_WINDOW=True
-
-  glass-input &
-  glass-widgets &
-  glass-ghosts &
-  glass-animator &
-  glass-renderer &
-  xterm
-
-else
-    
-  xterm -geometry 400x100+0+0 -e '
 export GLASS_SHADER_PATH=/usr/local/share/glass
-export GLASS_ERROR_EXIT=True
-export GLASS_ERROR_BAD_WINDOW=True
-  
-glass-input &
-glass-widgets &
-glass-ghosts &
-glass-animator &
-glass-renderer &
-bash
-'
 
-fi
+session_content() {
+    glass-input &
+    glass-widgets &
+    glass-animator &
+    glass-renderer &
+    xterm
+}
+
+glass-ghosts | { read SESSION_MANAGER; export SESSION_MANAGER; session_content; }
+
 
