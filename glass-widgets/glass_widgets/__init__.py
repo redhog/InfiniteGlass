bb0499095e817e657912b282d814eff9a7039d1a
Egil <egil.moller@freecode.no>
2019-11-08 11:45:40 +0100

    Set WM_NAME/WM_CLASS for widgets

--- glass-widgets/glass_widgets/__init__.py
+++ glass-widgets/glass_widgets/__init__.py
@@ -9,6 +9,8 @@ def main(*arg, **kw):
                                             ("search-location", -1.0))):
             w = display.root.create_window()
             w.zoom = zoom
+            w["WM_NAME"]=icon.encode("utf-8")
+            w["WM_CLASS"]=b"glass-widget"
             w["IG_LAYER"]="IG_LAYER_OVERLAY"
             w["IG_COORDS"]=[0.01, 0.60+0.05*idx, 0.05, 0.05]
             with pkg_resources.resource_stream("glass_widgets", "fontawesome-free-5.9.0-desktop/svgs/solid/%s.svg" % icon) as f:
@@ -28,6 +30,8 @@ def main(*arg, **kw):
                 display.root["IG_VIEW_DESKTOP_VIEW"] = screen
 
         w = display.root.create_window()
+        w["WM_NAME"]=b"eject"
+        w["WM_CLASS"]=b"glass-widget"
         w["IG_LAYER"]="IG_LAYER_OVERLAY"
         w["IG_COORDS"]=[0.01,0.55,0.05,0.05]
         with pkg_resources.resource_stream("glass_widgets", "fontawesome-free-5.9.0-desktop/svgs/solid/eject.svg") as f:
