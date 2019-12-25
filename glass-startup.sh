77b25df90eaa81d6b48cb21758d7a790f03d2dcc
Egil <egil.moller@freecode.no>
2019-12-26 00:58:33 +0100

    Start all components from ghosts

--- glass-startup.sh
+++ glass-startup.sh
@@ -2,21 +2,4 @@
 
 exec > /tmp/glass.log 2>&1
 
-TMPFILE="$(tempfile)"
-FIFO="$TMPFILE.fifo"
-
-mkfifo "$FIFO"
-
-glass-ghosts | { read x; echo $x > "$FIFO"; cat; } &
-
-export SESSION_MANAGER="$(cat $FIFO)"
-
-glass-input &
-glass-theme &
-glass-widgets &
-glass-animator &
-glass-renderer &
-
-xterm
-
-rm "$FIFO" "$TMPFILE"
+glass-ghosts
