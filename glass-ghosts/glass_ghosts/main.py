da9676f7997d688343388913a814c4b599e2567a
Egil <egil.moller@freecode.no>
2020-01-12 21:32:07 +0100

    Restart glass-ghosts on global exceptions

--- glass-ghosts/glass_ghosts/main.py
+++ glass-ghosts/glass_ghosts/main.py
@@ -3,8 +3,11 @@ import glass_ghosts.manager
 import glass_ghosts.session
 import sys
 import traceback
+import signal
+import os
 
 def main2():
+    manager = None
     try:
         with InfiniteGlass.Display() as display:
             manager = glass_ghosts.manager.GhostManager(display)
@@ -12,8 +15,16 @@ def main2():
             sys.stdout.flush()
             InfiniteGlass.DEBUG("init", "Session manager listening to %s\n" % manager.session.listen_address())
     except Exception as e:
-        print(e)
+        print("Ghost manager systemic failure, restarting: %s" % (e,))
         traceback.print_exc()
+        try:
+            if manager is not None and hasattr(manager, "components") and hasattr(manager.components, "components_by_pid"):
+                for pid in manager.components.components_by_pid.keys():
+                    os.kill(pid, signal.SIGINT)
+        except Exception as e:
+            print(e)
+            traceback.print_exc()
+        os.execlp(sys.argv[0], *sys.argv)        
     print("END")
 
 def main():
