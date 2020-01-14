1f43afbe1d6c701a1e5a4b02714b38ccad883f05
Egil <egil.moller@freecode.no>
2020-01-14 13:51:33 +0100

    Draw a rainbow serpent

--- glass-theme/glass_theme/__init__.py
+++ glass-theme/glass_theme/__init__.py
@@ -1,6 +1,27 @@
 import InfiniteGlass
 import pkg_resources
+import json
+import numpy
 
+def linestrings2texture(f):
+    coastline = json.load(f)
+
+    size = 0
+    count = 0
+    for feature in coastline["features"]:
+        size += len(feature["geometry"]["coordinates"]) * 2
+        count += 1
+
+    data = numpy.zeros(size + count)
+    pos = 0
+    for feature in coastline["features"]:
+        coords = [item for coord in feature["geometry"]["coordinates"] for item in coord]
+        data[pos] = len(coords)
+        data[pos+1:] = coords
+        pos += 1 + len(coords)
+
+    return data.tobytes()
+        
 def main(*arg, **kw):
     with InfiniteGlass.Display() as display:
         display.root["IG_VIEW_MENU_LAYER"] = "IG_LAYER_MENU"
@@ -20,13 +41,6 @@ def main(*arg, **kw):
         with pkg_resources.resource_stream("glass_theme", "shader_window_fragment.glsl") as f:
             display.root["IG_SHADER_DEFAULT_FRAGMENT"] = f.read()
 
-        with pkg_resources.resource_stream("glass_theme", "shader_window_geometry.glsl") as f:
-            display.root["IG_SHADER_TEST_GEOMETRY"] = f.read()
-        with pkg_resources.resource_stream("glass_theme", "shader_window_vertex.glsl") as f:
-            display.root["IG_SHADER_TEST_VERTEX"] = f.read()
-        with pkg_resources.resource_stream("glass_theme", "shader_window_fragment_test.glsl") as f:
-            display.root["IG_SHADER_TEST_FRAGMENT"] = f.read()
-
         with pkg_resources.resource_stream("glass_theme", "shader_root_geometry.glsl") as f:
             display.root["IG_SHADER_ROOT_GEOMETRY"] = f.read()
         with pkg_resources.resource_stream("glass_theme", "shader_root_vertex.glsl") as f:
@@ -34,8 +48,18 @@ def main(*arg, **kw):
         with pkg_resources.resource_stream("glass_theme", "shader_root_fragment.glsl") as f:
             display.root["IG_SHADER_ROOT_FRAGMENT"] = f.read()
 
-        display.root["IG_SHADERS"] = ["IG_SHADER_ROOT", "IG_SHADER_DEFAULT", "IG_SHADER_TEST"]
+        display.root["IG_SHADERS"] = ["IG_SHADER_ROOT", "IG_SHADER_DEFAULT"]
 
+        #with pkg_resources.resource_stream("glass_theme", "coastline50.geojson") as f:
+        #    display.root["IG_COASTLINE"] = linestrings2texture(f)
+
+        display.root["IG_DRAW_TYPE"] = "IG_DRAW_TYPE_LINE_STRIP"
+        display.root["SERPENT"] = [
+            c for s in range(9, 0, -1) for c in [-.1 * s, -.1 * (s + 1),
+                                                 -.1 * s, .1 *s,
+                                                 .1 * s, .1 * s,
+                                                 .1 * s, -.1 * s]]
+        
         InfiniteGlass.DEBUG("init", "Theme started\n")
 
         display.sync()
--- glass-theme/glass_theme/shader_root_geometry.glsl
+++ glass-theme/glass_theme/shader_root_geometry.glsl
@@ -1,26 +1,19 @@
 #version 330 core
 
-layout(points) in;
-layout(triangle_strip, max_vertices=4) out;
+layout(lines) in;
+layout(line_strip, max_vertices=4) out;
 
 uniform ivec2 size;
 
+in vec2 serpent[];
 out vec2 px_coord;
 
 void main() {
-
-  gl_Position = vec4(-1., -1., 0., 1.);
-  px_coord = vec2(0., size.y);
-  EmitVertex();  
-  gl_Position = vec4(-1., 1., 0., 1.);
-  px_coord = vec2(0., 0.);
+  gl_Position = vec4(serpent[0].x, serpent[0].y, 0., 1.);
+  px_coord = vec2(size.x * (serpent[0].x + 1. / 2.), size.y * (1. - (serpent[0].y + 1. / 2.)));
   EmitVertex();
-  gl_Position = vec4(1., -1., 0., 1.);
-  px_coord = vec2(size.x, size.y);
+  gl_Position = vec4(serpent[1].x, serpent[1].y, 0., 1.);
+  px_coord = vec2(size.x * (serpent[1].x + 1. / 2.), size.y * (1. - (serpent[1].y + 1. / 2.)));
   EmitVertex();
-  gl_Position = vec4(1., 1., 0., 1.);
-  px_coord = vec2(size.x, 0.);
-  EmitVertex();
-
   EndPrimitive();
 }
