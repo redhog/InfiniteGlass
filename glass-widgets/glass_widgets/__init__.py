8efcb75267cad89886687a06ac77db3e864aba0e
Egil <egil.moller@freecode.no>
2020-02-27 12:53:22 +0100

    Window decoration support in widget config

--- glass-widgets/glass_widgets/__init__.py
+++ glass-widgets/glass_widgets/__init__.py
@@ -17,30 +17,36 @@ def main(*arg, **kw):
         config = yaml.load(f, Loader=yaml.SafeLoader)
             
     with InfiniteGlass.Display() as display:
-        for name, widget in config["widgets"].items():
-            w = display.root.create_window()
+        for widget_type, widgets in config.items():
+            for name, widget in config["widgets"].items():
+                w = display.root.create_window()
 
-            properties = {
-                "WM_CLASS": "data://glass-widget",
-                "IG_LAYER": "IG_LAYER_OVERLAY",
-                "_NET_WM_WINDOW_TYPE": "_NET_WM_WINDOW_TYPE_DESKTOP"
-            }
-            properties.update(widget["properties"])
-            properties = json.loads(
-                json.dumps(properties),
-                object_hook=InfiniteGlass.fromjson(display))
-            
-            for key, value in properties.items():
-                w[key] = value
+                properties = {
+                    "WM_CLASS": "data://glass-widget",
+                    "_NET_WM_WINDOW_TYPE": "_NET_WM_WINDOW_TYPE_DESKTOP"
+                }
+                if widget_type == "widgets":
+                    properties["IG_LAYER"] = "IG_LAYER_OVERLAY"
+                elif widget_type == "window-decorations":
+                    properties["IG_LAYER"] = "IG_LAYER_NONE"
+                    #properties["IG_SHADER"] = "IG_SHADER_WINDOW_DECORATION"
+                    display.root["IG_WINDOW_DECORATION_" + name.upper()] = ("IG_ITEM", w)
+                properties.update(widget["properties"])
+                properties = json.loads(
+                    json.dumps(properties),
+                    object_hook=InfiniteGlass.fromjson(display))
 
-            w.widget = widget
-            
-            @w.on()
-            def ButtonPress(win, event):
-                win["IG_INPUT_ACTION"] = json.dumps(win.widget["action"]).encode("utf-8")
-                display.root.send(display.root,
-                                  "IG_INPUT_ACTION", win, "IG_INPUT_ACTION",
-                                  event_mask=Xlib.X.SubstructureNotifyMask|Xlib.X.SubstructureRedirectMask)
-                display.flush()
+                for key, value in properties.items():
+                    w[key] = value
+
+                w.widget = widget
+
+                @w.on()
+                def ButtonPress(win, event):
+                    win["IG_INPUT_ACTION"] = json.dumps(win.widget["action"]).encode("utf-8")
+                    display.root.send(display.root,
+                                      "IG_INPUT_ACTION", win, "IG_INPUT_ACTION",
+                                      event_mask=Xlib.X.SubstructureNotifyMask|Xlib.X.SubstructureRedirectMask)
+                    display.flush()
                 
         InfiniteGlass.DEBUG("init", "Widgets started\n")
