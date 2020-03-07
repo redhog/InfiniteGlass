61f4bbd901305efe08a6899caadb33848fe368e7
Egil <egil.moller@freecode.no>
2020-03-07 15:22:08 +0100

    Different save/restore icons

--- glass-theme/glass_theme/shaders/decoration/fragment.glsl
+++ glass-theme/glass_theme/shaders/decoration/fragment.glsl
@@ -12,22 +12,34 @@ in vec2 px_coord;
 uniform ivec2 size;
 uniform int atom_IG_LAYER_DESKTOP;
 
+uniform int atom_IG_DECORATION_SAVE;
+uniform int atom_IG_DECORATION_RESTORE;
+uniform int IG_DECORATION;
+
+uniform int atom_IG_GHOST;
+uniform int parent_IG_GHOST;
+
 out vec4 fragColor;
 
 void main() {
-  vec2 window_coord = px_coord - px_window_bottom_left;
-  vec2 window_size = px_window_top_right - px_window_bottom_left;
-  vec2 scaled_window_coord = vec2(window_coord.x / window_size.x, 1. - window_coord.y / window_size.y);
-
-  vec2 texture_size = textureSize(window_sampler, 0);
-  vec2 geometry_size = window_size / texture_size;
-  float geometry_scale = sqrt(pow(geometry_size.x, 2) + pow(geometry_size.y, 2));
-
-  if (picking_mode == 1) {
-    fragColor = get_picking(window_id, scaled_window_coord);
-  } else if (!isnan(IG_CONTENT_transform[0])) {
-    fragColor = get_svg_content(scaled_window_coord);
-  } else {
+  if (   ((IG_DECORATION == atom_IG_DECORATION_SAVE) && (parent_IG_GHOST == atom_IG_GHOST))
+      || ((IG_DECORATION == atom_IG_DECORATION_RESTORE) && (parent_IG_GHOST != atom_IG_GHOST))) {
     fragColor = vec4(0., 0., 0., 0.);
+  } else {
+    vec2 window_coord = px_coord - px_window_bottom_left;
+    vec2 window_size = px_window_top_right - px_window_bottom_left;
+    vec2 scaled_window_coord = vec2(window_coord.x / window_size.x, 1. - window_coord.y / window_size.y);
+
+    vec2 texture_size = textureSize(window_sampler, 0);
+    vec2 geometry_size = window_size / texture_size;
+    float geometry_scale = sqrt(pow(geometry_size.x, 2) + pow(geometry_size.y, 2));
+
+    if (picking_mode == 1) {
+      fragColor = get_picking(window_id, scaled_window_coord);
+    } else if (!isnan(IG_CONTENT_transform[0])) {
+      fragColor = get_svg_content(scaled_window_coord);
+    } else {
+      fragColor = vec4(0., 0., 0., 0.);
+    }
   }
 }
