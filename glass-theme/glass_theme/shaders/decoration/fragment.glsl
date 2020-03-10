80ea50ded46dd03611a09027f9b8ac77204c73dc
Egil <egil.moller@freecode.no>
2020-03-10 23:54:49 +0100

    Only show decorations when mouse is near

--- glass-theme/glass_theme/shaders/decoration/fragment.glsl
+++ glass-theme/glass_theme/shaders/decoration/fragment.glsl
@@ -13,6 +13,7 @@ in vec2 px_parent_window_bottom_left;
 in vec2 px_parent_window_top_right;
 
 uniform ivec2 size;
+uniform ivec2 pointer;
 uniform int atom_IG_LAYER_DESKTOP;
 
 uniform int atom_IG_DECORATION_SAVE;
@@ -37,14 +38,23 @@ void main() {
   vec2 geometry_size = parent_window_size / size;
   float geometry_scale = sqrt(pow(geometry_size.x, 2) + pow(geometry_size.y, 2));
 
-  if (geometry_scale < 0.25) {
+  vec2 center = (px_window_top_right + px_window_bottom_left) / 2;
+
+  float mouse_dist = sqrt(pow(center.x - pointer.x, 2) + pow(center.y - pointer.y, 2));
+
+  if (geometry_scale < 0.25 || mouse_dist > 150.0) {
     fragColor = vec4(0., 0., 0., 0.);
   } else if (picking_mode == 1) {
     fragColor = get_picking(window_id, scaled_window_coord);
-  } else if (   ((IG_DECORATION == atom_IG_DECORATION_SAVE) && (parent_IG_GHOST == atom_IG_GHOST))
-             || ((IG_DECORATION == atom_IG_DECORATION_GHOSTS_ENABLE) && (parent_IG_GHOSTS_DISABLED != 1))) {
-    fragColor = get_svg_content_any(scaled_window_coord, IG_CONTENT_ALT, IG_CONTENT_ALT_transform);
   } else {
-    fragColor = get_svg_content(scaled_window_coord);
+    if (   ((IG_DECORATION == atom_IG_DECORATION_SAVE) && (parent_IG_GHOST == atom_IG_GHOST))
+               || ((IG_DECORATION == atom_IG_DECORATION_GHOSTS_ENABLE) && (parent_IG_GHOSTS_DISABLED != 1))) {
+      fragColor = get_svg_content_any(scaled_window_coord, IG_CONTENT_ALT, IG_CONTENT_ALT_transform);
+    } else {
+      fragColor = get_svg_content(scaled_window_coord);
+    }
+    if (mouse_dist > 100.0) {
+      fragColor.a *= 1. - (mouse_dist - 100.) / 50.;
+    }
   }
 }
