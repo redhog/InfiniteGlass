4c3508b14ede0f1cf2e3410f9dabfeb0e302e8dd
Egil <egil.moller@freecode.no>
2020-04-11 00:36:58 +0200

    Correct loading of views properties

--- glass-renderer/property_views.c
+++ glass-renderer/property_views.c
@@ -1,5 +1,7 @@
 #include "property_views.h"
 #include "property_atom.h"
+#include "property_float.h"
+#include "property_int.h"
 #include "view.h"
 #include "wm.h"
 #include "debug.h"
@@ -12,7 +14,15 @@ int property_views_match(Property *prop) {
 }
  
 void property_views_load(Property *prop) {
-  if (prop->window != root) return;
+  if (prop->type == XA_ATOM) {
+    property_atom_load(prop);
+  } else if (prop->type == XA_FLOAT) {
+    // property_float_load(prop);
+  } else if (prop->type == XA_INTEGER) {
+    property_int_load(prop);
+  }
+
+  if (prop->window != root) return; // This shouldn't happen, right?
   if (prop->name == ATOM("IG_VIEWS")) {
     view_free_all(views);
     views = view_load_all();
@@ -30,12 +40,29 @@ void property_views_load(Property *prop) {
   }
 }
 
+void property_views_free(Property *prop) {
+  if (prop->type == XA_ATOM) {
+    property_atom_free(prop);
+  } else if (prop->type == XA_FLOAT) {
+   // property_float_free(prop);
+  } else if (prop->type == XA_INTEGER) {
+    property_int_free(prop);
+  }
+}
+
+void property_views_print(Property *prop, FILE *fp) {
+  if (prop->type == XA_ATOM) {
+    property_atom_print(prop, fp);
+  } else if (prop->type == XA_FLOAT) {
+    property_float_print(prop, fp);
+  } else if (prop->type == XA_INTEGER) {
+    property_int_print(prop, fp);
+  }
+}
+
 PropertyTypeHandler property_views = {
   .match=&property_views_match,
   .load=&property_views_load,
-  .free=&property_atom_free,
-  .to_gl=&property_atom_to_gl,
-  .print=&property_atom_print,
-  .load_program=&property_atom_load_program,
-  .free_program=&property_atom_free_program
+  .free=&property_views_free,
+  .print=&property_views_print,
 };
