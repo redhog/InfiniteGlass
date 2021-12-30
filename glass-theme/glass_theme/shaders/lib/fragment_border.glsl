99ed03f2cbfba38c25e6ad620882a40fc1108d86
Egil <egil.moller@freecode.no>
2021-12-30 01:19:44 +0100

    Simpler / better looking islands

--- glass-theme/glass_theme/shaders/lib/fragment_border.glsl
+++ glass-theme/glass_theme/shaders/lib/fragment_border.glsl
@@ -6,6 +6,9 @@ uniform int atom__NET_WM_WINDOW_TYPE_NORMAL;
 uniform int _NET_WM_WINDOW_TYPE;
 uniform int root__NET_ACTIVE_WINDOW;
 
+uniform int IG_WINDOW_TYPE;
+uniform int atom_IG_ISLAND;
+
 vec4 get_border(int window_id, vec2 window_coord, vec2 window_size) {
   vec2 dcoord = window_coord;
   if (dcoord.x >= 0. && dcoord.x < window_size.x) dcoord.x = 0.;
@@ -14,7 +17,7 @@ vec4 get_border(int window_id, vec2 window_coord, vec2 window_size) {
   if (dcoord.y >= window_size.y) dcoord.y = dcoord.y - window_size.y;
   vec2 dist = ceil(abs(dcoord));
 
-  if (IG_LAYER == atom_IG_LAYER_MENU || (_NET_WM_WINDOW_TYPE != 0 &&_NET_WM_WINDOW_TYPE != atom__NET_WM_WINDOW_TYPE_NORMAL)) {
+  if (IG_LAYER == atom_IG_LAYER_MENU || (_NET_WM_WINDOW_TYPE != 0 && _NET_WM_WINDOW_TYPE != atom__NET_WM_WINDOW_TYPE_NORMAL && IG_WINDOW_TYPE != atom_IG_ISLAND)) {
     return vec4(0., 0., 0., 0.);
   } else if (root__NET_ACTIVE_WINDOW == window_id) {
     if ((dist.x == 3 && dist.y < 4) || (dist.y == 3 && dist.x < 4)) {
