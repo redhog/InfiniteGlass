3e6e064223b217692b27abb04e4f9656f59c62ec
Egil <egil.moller@freecode.no>
2022-02-20 16:41:07 +0100

    Bugfix

--- glass-lib/InfiniteGlass/profile.py
+++ glass-lib/InfiniteGlass/profile.py
@@ -6,17 +6,13 @@ def profilable(filename):
         if not debug.FUNCTIONALITY_ENABLED("GLASS_PROFILE", fn=fn):
             return fn
         def wrapper(*arg, **kw):
-            print("AAAAAAAAAAAA1", fn.__module__, fn.__name__, arg, kw)
             with cProfile.Profile() as pr:
-                print("AAAAAAAAAAAA2", fn.__module__, fn.__name__, arg, kw)
                 res = fn(*arg, **kw)
-                print("AAAAAAAAAAAA3", fn.__module__, fn.__name__, arg, kw)
-            if not isinstance(filename, str):
-                filename = "%s.%s.prof" % (filename.__module__, filename.__name__)
             pr.dump_stats(filename)
             return res
         return wrapper
     if isinstance(filename, str):
         return profilable
-    print("XXXXXXXXXXXX", filename.__module__, filename.__name__)
-    return profilable(filename)
+    fn = filename
+    filename = "%s.%s.prof" % (fn.__module__, fn.__name__)
+    return profilable(fn)
