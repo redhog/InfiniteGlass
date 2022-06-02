14fad68a28bd66b1e37deb7cba9f5ae3c22e8b67
Egil <egil.moller@freecode.no>
2022-06-02 23:08:55 +0200

    glass-action run -i

--- glass-action/glass_action/run.py
+++ glass-action/glass_action/run.py
@@ -4,19 +4,34 @@ import glass_action.main
 import distutils
 
 @glass_action.main.main.command()
-@click.option('--restartable', '-r', is_flag=True)
-@click.option('--set', '-s', multiple=True)
+@click.option('--id', '-i', help="Set the application id for this instance of the app")
+@click.option('--app', '-a', multiple=True, help="PROPNAME=value - set a property on the windows of an app")
+@click.option('--group', '-g', multiple=True, help="PROPNAME=value - set a property on the windows of an app and all its children")
+@click.option('--restartable', '-r', is_flag=True, help="Set WM_COMMAND")
 @click.argument('arguments', nargs=-1)
 @click.pass_context
-def run(ctx, restartable, set, arguments):
+def run(ctx, id=None, app=[], group=[], restartable=False, arguments=[]):
+    """Run an X application and annotate all its windows with extra properties.
+Optionally sets WM_COMMAND for the application with a unique application ID.
+Can set some properties on windows of all child processes too.
+
+Property values can be any string, or __WM_COMMAND__ or __APPID__.
+
+ARGUMENTS is the command to run, prefixed by --
+"""
     env = dict(os.environ)
     preload = [distutils.spawn.find_executable('glass-annotator')]
     if "LD_PRELOAD" in env:
         preload.extend(env["LD_PRELOAD"].split(" "))
     env["LD_PRELOAD"] = " ".join(preload)
-    for pair in set:
+    if id is not None:
+        env["IG_APPID"] = id
+    for pair in app:
         name, value = pair.split("=")
-        env["IG_ANNOTATE_" + name] = value
+        env["IG_APP_" + name] = value
+    for pair in group:
+        name, value = pair.split("=")
+        env["IG_GROUP_" + name] = value
     if restartable:
-        env["IG_ANNOTATE_WM_COMMAND"] = "__WM_COMMAND__"
+        env["IG_APP_WM_COMMAND"] = "__WM_COMMAND__"
     os.execvpe(arguments[0], arguments, env)
--- glass-annotator/xlib_wrapper.c
+++ glass-annotator/xlib_wrapper.c
@@ -7,6 +7,10 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <X11/Xlib.h>
+#include <sys/random.h>
+
+#define TYPE_APP 0
+#define TYPE_GROUP 1
 
 extern char **environ;
 static char **props;
@@ -14,36 +18,62 @@ static size_t propslen;
 static char *args;
 static size_t argslen;
 
+static char app_id[17];
+static void generate_app_id() {
+  unsigned int seed;
+  getrandom((void *) &seed, sizeof(seed), 0);
+  srandom(seed);
+  const char charset[] = "0123456789ABCDEF";
+  for (size_t n = 0; n < 16; n++) {
+    int key = random() % (int) (sizeof(charset) - 1);
+    app_id[n] = charset[key];
+  }
+  app_id[16] = '\0';
+}
+
 Window (*XCreateWindow_orig)(Display *display, Window parent, int x, int y, unsigned int width, unsigned int height, unsigned int border_width,
                              int depth, unsigned int class, Visual *visual, unsigned long valuemask, XSetWindowAttributes *attributes) = NULL;
 pid_t (*fork_orig)(void);
 
-int xlib_wrapper_start(int argc, char **argv, char **env) {
-  XCreateWindow_orig = dlsym(RTLD_NEXT, "XCreateWindow");
-  fork_orig = dlsym(RTLD_NEXT, "fork");
-  
+void set_props() {
   propslen = 0;
   for (char **e = environ; *e; e++) {
-    if (strncmp("IG_ANNOTATE_", *e, strlen("IG_ANNOTATE_")) == 0) {
+   if (   (strncmp("IG_APP_", *e, strlen("IG_APP_")) == 0)
+       || (strncmp("IG_GROUP_", *e, strlen("IG_GROUP_")) == 0)) {
       propslen++;
     }
   }
   props = malloc(sizeof(char *) * (propslen + 1));
   char **p = props;
   for (char **e = environ; *e; e++) {
-   if (strncmp("IG_ANNOTATE_", *e, strlen("IG_ANNOTATE_")) == 0) {
-      *p = malloc(strlen(*e) - strlen("IG_ANNOTATE_") + 1);
-      strcpy(*p, *e + strlen("IG_ANNOTATE_"));
+    if (strncmp("IG_APP_", *e, strlen("IG_APP_")) == 0) {
+      *p = malloc(1 + strlen(*e) - strlen("IG_APP_") + 1);
+      **p = TYPE_APP;
+      strcpy(*p + 1, *e + strlen("IG_APP_"));
       char *sep = strchr(*p, '=');
       if (sep) *sep = '\0';
       p++;
+    } else if (strncmp("IG_GROUP_", *e, strlen("IG_GROUP_")) == 0) {
+      *p = malloc(1 + strlen(*e) - strlen("IG_GROUP_") + 1);
+      **p = TYPE_GROUP;
+      strcpy(*p + 1, *e + strlen("IG_GROUP_"));
+      char *sep = strchr(*p, '=');
+      if (sep) *sep = '\0';
+      p++;
+    } else if (strncmp("IG_APPID=", *e, strlen("IG_APPID=")) == 0) {
+      strncpy(app_id, *e + strlen("IG_APPID="), sizeof(app_id) - 1); 
     }
   }
   *p = NULL;
+}
 
+void set_args(char **argv) {
   argslen = 0;
-  argslen += strlen("glass-action") + 1;
-  argslen += strlen("run") + 1;
+  argslen += sizeof("glass-action");
+  argslen += sizeof("run");
+  argslen += sizeof("-i");
+  argslen += sizeof(app_id);
+
   for (char **p = props; *p; p++) {
     int namelen = strlen(*p);
     char * value = *p + namelen + 1;
@@ -57,17 +87,26 @@ int xlib_wrapper_start(int argc, char **argv, char **env) {
   args = malloc(argslen);
   char *b = args;
   strcpy(b, "glass-action");
-  b += strlen("glass-action") + 1;
+  b += sizeof("glass-action");
   strcpy(b, "run");
-  b += strlen("run") + 1;
+  b += sizeof("run");
+  strcpy(b, "-i");
+  b += 3;
+  strcpy(b, app_id);
+  b += sizeof(app_id);
   for (char **p = props; *p; p++) {
-    int namelen = strlen(*p);
-    char *value = *p + namelen + 1;
+    char *name = *p + 1;
+    int namelen = strlen(name);
+    char *value = *p + 1 + namelen + 1;
     int valuelen = strlen(value);
 
-    strcpy(b, "-s");
+    if (**p == TYPE_APP) {
+      strcpy(b, "-a");
+    } if (**p == TYPE_GROUP) {
+      strcpy(b, "-g");
+    }
     b += 3;
-    strcpy(b, *p);
+    strcpy(b, name);
     b+= namelen;
     *b = '=';
     b++;
@@ -79,6 +118,15 @@ int xlib_wrapper_start(int argc, char **argv, char **env) {
     strcpy(b, *a);
     b += strlen(*a) + 1;
   }
+}
+
+int xlib_wrapper_start(int argc, char **argv, char **env) {
+  XCreateWindow_orig = dlsym(RTLD_NEXT, "XCreateWindow");
+  fork_orig = dlsym(RTLD_NEXT, "fork");
+
+  generate_app_id();
+  set_props();
+  set_args(argv);
   
   return 0;
 }
@@ -87,20 +135,23 @@ __attribute__((section(".init_array"))) void *xlib_wrapper_start_constructor = &
 Window XCreateWindow(Display *display, Window parent, int x, int y, unsigned int width, unsigned int height, unsigned int border_width,
                      int depth, unsigned int class, Visual *visual, unsigned long valuemask, XSetWindowAttributes *attributes) {
   Window window;
- 
 
   window = XCreateWindow_orig(display, parent, x, y, width, height, border_width,
                               depth, class, visual, valuemask, attributes);
 
   Atom prop_type = XInternAtom(display, "STRING", False);
   for (char **p = props; *p; p++) {
-    char *value = *p + strlen(*p) + 1;
+    char *name = *p + 1;
+    char *value = name + strlen(name) + 1;
     int valuelen = strlen(value);
     if (strcmp(value, "__WM_COMMAND__") == 0) {
       value = args;
       valuelen = argslen;
+    } else if (strcmp(value, "__APPID__") == 0) {
+      value = app_id;
+      valuelen = sizeof(app_id) - 1;
     }
-    Atom prop_name = XInternAtom(display, *p, False);
+    Atom prop_name = XInternAtom(display, name, False);
     XChangeProperty(display, window, prop_name, prop_type, 8, PropModeReplace, (const unsigned char *) value, valuelen);
   }
   
@@ -113,7 +164,8 @@ pid_t fork(void) {
     char **e_out = environ;
     for (char **e_in = environ; *e_in; e_in++) {
       *e_out = *e_in;
-      if (strncmp("IG_ANNOTATE_", *e_in, strlen("IG_ANNOTATE_")) != 0) {
+      if (   (strncmp("IG_APP_", *e_in, strlen("IG_APP_")) != 0)
+          && (strcmp("IG_APPID", *e_in) != 0)) {
         e_out++;
       }
     }
