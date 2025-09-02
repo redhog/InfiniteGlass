47d67274c3c71640cc512d13fe040aee402cf29f
Egil <egil.moller@freecode.no>
2025-09-02 22:30:13 +0200

    Bugfix for ifnowin infinite loop

--- glass-input/glass_input/actions/modeswitch.py
+++ glass-input/glass_input/actions/modeswitch.py
@@ -5,4 +5,5 @@ def ifnowin(self, event, mode=None):
         modemod.pop(self.display)
         if mode is not None:
             modemod.push_by_name(self.display, mode, first_event=self.first_event, last_event=self.last_event)
-
+        else:
+            raise Exception("No window selected, and no alternative mode to fall back to")
