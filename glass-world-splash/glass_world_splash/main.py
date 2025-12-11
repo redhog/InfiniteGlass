fe7d9b0b194efb65f3849b6407f1b414d58c319e
Egil Moeller <redhog@redhog.org>
2025-12-11 17:33:59 +0100

    Restore previous desktop on unlock, and speed up the zoom

--- glass-world-splash/glass_world_splash/main.py
+++ glass-world-splash/glass_world_splash/main.py
@@ -38,7 +38,7 @@ def create_splash_windows(display, coastline="resource://glass_theme/coastline50
     
     return w1, w2
 
-def splash_unlock(self, **kw):
+def splash_unlock(self, timeframe = 5.0, **kw):
     latlon = self.display.root.get("IG_WORLD_COORDS", [70., 18.])
     if len(latlon) == 2:
         latlon = [latlon[0]-180, latlon[1]-360] + latlon
@@ -49,7 +49,10 @@ def splash_unlock(self, **kw):
 
     geom = self.display.root.get_geometry()
     height = float(geom.height) / float(geom.width)
+
+    view_orig = self.display.root.get("IG_VIEW_DESKTOP_VIEW_ORIG", [0.0, 0.0, 1.0, height])
     
+    timeframe = timeframe / 5. # Sum of all timeframes
     self.display.root["IG_INITIAL_ANIMATE"] = {
         "steps": [
             {"window": self.display.root, "atom": "IG_WORLD_ZOOM", "dst": 0.1},
@@ -60,14 +63,14 @@ def splash_unlock(self, **kw):
             {"window": w2, "atom": "IG_LAYER", "dst": "IG_LAYER_SPLASH_BACKGROUND"},
             {"window": self.display.root, "atom": "IG_VIEW_DESKTOP_VIEW", "dst": [-50.0, height * -50.0, 100.0, height * 100.0]},
             {"tasks": [
-                {"window": self.display.root, "atom": "IG_WORLD_LAT", "timeframe": 3.0, "dst": lat, "easing": "OutCubic"},
-                {"window": self.display.root, "atom": "IG_WORLD_LON", "timeframe": 3.0, "dst": lon, "easing": "OutCubic"},
-                {"window": self.display.root, "atom": "IG_WORLD_ZOOM", "timeframe": 3.0, "dst": 40.0, "easing": "InCubic"}
+                {"window": self.display.root, "atom": "IG_WORLD_LAT", "timeframe": timeframe * 3.0, "dst": lat, "easing": "OutCubic"},
+                {"window": self.display.root, "atom": "IG_WORLD_LON", "timeframe": timeframe * 3.0, "dst": lon, "easing": "OutCubic"},
+                {"window": self.display.root, "atom": "IG_WORLD_ZOOM", "timeframe": timeframe * 3.0, "dst": 40.0, "easing": "InCubic"}
             ]},
             {"tasks": [
-                {"window": self.display.root, "atom": "IG_WORLD_ALPHA", "timeframe": 2.0, "dst": 0.0},
-                {"window": self.display.root, "atom": "IG_WORLD_ZOOM", "timeframe": 2.0, "dst": 400.0},
-                {"window": self.display.root, "atom": "IG_VIEW_DESKTOP_VIEW", "timeframe": 2.0, "dst": [0.0, 0.0, 1.0, height]}
+                {"window": self.display.root, "atom": "IG_WORLD_ALPHA", "timeframe": timeframe * 2.0, "dst": 0.0},
+                {"window": self.display.root, "atom": "IG_WORLD_ZOOM", "timeframe": timeframe * 2.0, "dst": 400.0},
+                {"window": self.display.root, "atom": "IG_VIEW_DESKTOP_VIEW", "timeframe": timeframe * 2.0, "dst": view_orig}
             ]},
             {"window": w1, "atom": "IG_LAYER", "dst": "IG_LAYER_NONE"},
             {"window": w2, "atom": "IG_LAYER", "dst": "IG_LAYER_NONE"},
@@ -77,7 +80,7 @@ def splash_unlock(self, **kw):
     anim.send(anim, "IG_ANIMATE", self.display.root, "IG_INITIAL", 0.0, event_mask=Xlib.X.PropertyChangeMask)
     
 
-def splash_lock(self, **kw):
+def splash_lock(self, timeframe=5.0, **kw):
     latlon = self.display.root.get("IG_WORLD_COORDS", [70., 18.])
     if len(latlon) == 2:
         latlon = [latlon[0]-180, latlon[1]-360] + latlon
@@ -88,7 +91,10 @@ def splash_lock(self, **kw):
 
     geom = self.display.root.get_geometry()
     height = float(geom.height) / float(geom.width)
+
+    self.display.root["IG_VIEW_DESKTOP_VIEW_ORIG"] = self.display.root["IG_VIEW_DESKTOP_VIEW"]
     
+    timeframe = timeframe / 5. # Sum of all timeframes
     self.display.root["IG_INITIAL_ANIMATE"] = {
         "steps": [
             {"window": self.display.root, "atom": "IG_WORLD_ZOOM", "dst": 400.0},
@@ -97,16 +103,15 @@ def splash_lock(self, **kw):
             {"window": self.display.root, "atom": "IG_WORLD_ALPHA", "dst": 0.0},
             {"window": w1, "atom": "IG_LAYER", "dst": "IG_LAYER_SPLASH"},
             {"window": w2, "atom": "IG_LAYER", "dst": "IG_LAYER_SPLASH_BACKGROUND"},
-            {"window": self.display.root, "atom": "IG_VIEW_DESKTOP_VIEW", "dst": [0.0, 0.0, 1.0, height]},            
             {"tasks": [
-                {"window": self.display.root, "atom": "IG_WORLD_ALPHA", "timeframe": 2.0, "dst": 1.0},
-                {"window": self.display.root, "atom": "IG_WORLD_ZOOM", "timeframe": 2.0, "dst": 40.0},
-                {"window": self.display.root, "atom": "IG_VIEW_DESKTOP_VIEW", "timeframe": 2.0, "dst": [-50.0, height * -50.0, 100.0, height * 100.0]}
+                {"window": self.display.root, "atom": "IG_WORLD_ALPHA", "timeframe": timeframe * 2.0, "dst": 1.0},
+                {"window": self.display.root, "atom": "IG_WORLD_ZOOM", "timeframe": timeframe * 2.0, "dst": 40.0},
+                {"window": self.display.root, "atom": "IG_VIEW_DESKTOP_VIEW", "timeframe": timeframe * 2.0, "dst": [-50.0, height * -50.0, 100.0, height * 100.0]}
             ]},
             {"tasks": [
-                {"window": self.display.root, "atom": "IG_WORLD_LAT", "timeframe": 3.0, "dst": start_lat, "easing": "InCubic"},
-                {"window": self.display.root, "atom": "IG_WORLD_LON", "timeframe": 3.0, "dst": start_lon, "easing": "InCubic"},
-                {"window": self.display.root, "atom": "IG_WORLD_ZOOM", "timeframe": 3.0, "dst": 0.1, "easing": "OutCubic"}
+                {"window": self.display.root, "atom": "IG_WORLD_LAT", "timeframe": timeframe * 3.0, "dst": start_lat, "easing": "InCubic"},
+                {"window": self.display.root, "atom": "IG_WORLD_LON", "timeframe": timeframe * 3.0, "dst": start_lon, "easing": "InCubic"},
+                {"window": self.display.root, "atom": "IG_WORLD_ZOOM", "timeframe": timeframe * 3.0, "dst": 0.1, "easing": "OutCubic"}
             ]},
         ]
     }
