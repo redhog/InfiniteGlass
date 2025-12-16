7623cb5518bada6ba3bdac072dc124896da6bf3b
Egil Moeller <redhog@redhog.org>
2025-12-16 13:35:45 +0100

    First parts of turning everything into pip packages

---
+++ glass-renderer/glass_renderer/__init__.py
@@ -0,0 +1,7 @@
+import os
+import sys
+from importlib.resources import files
+
+def main():
+    bin_path = str(files("glass_renderer.bin") / "glass-renderer")    
+    os.execv(bin_path, [bin_path] + sys.argv[1:])
